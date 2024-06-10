854a9afde651ed608a1fce06188ee8e9e46f410f
Mathieu Keca <mathieu.keca@gmail.com>
2024-06-10 12:29:49 +0200

    arrangement dossiers/fichiers

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-import MiniReactDom from "./core/MiniReactDom.js";
+import MiniReactDom from "../core/MiniReactDom.js";
 import routes from "./routes.js";
 
 MiniReactDom.render(document.getElementById("root"), routes);
