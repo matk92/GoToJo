d580273433dcc9e2ce7f9512e77c412edddb1a3a
Mathieu K <mathieu.keca@gmail.com>
2024-06-25 16:43:18 +0200

    js

--- js/routes.js
+++ js/routes.js
@@ -3,11 +3,15 @@ import page2 from "../pages/page2.js";
 import landing from "../pages/landing.js";
 import home from "../pages/home.js";
 import medals from "../pages/medals.js";
+import calendar from "../pages/calendar.js";
+//import Page404 from '../pages/Page404.js';
 
 export default {
   "/": home,
   "/landing": landing,
+  "/calendar": calendar,
   "/medals": medals,
   "/page1": page1,
   "/page2": page2,
+  //"*": Page404,
 };
