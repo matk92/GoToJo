7e05341169e06504d1314fb7b2e30f5a38951859
Mathieu Keca <mathieu.keca@gmail.com>
2024-07-23 13:34:42 +0200

    countdown + calc.js

--- pages/home.js
+++ pages/home.js
@@ -44,6 +44,30 @@ export default function Home() {
     })
     .catch(console.error);
 
+  const targetDate = new Date("July 26, 2024 19:30:00").getTime();
+
+  function updateCountdown() {
+    const now = new Date().getTime();
+    const distance = targetDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerText = days;
+    document.getElementById("hours").innerText = hours;
+    document.getElementById("minutes").innerText = minutes;
+    document.getElementById("seconds").innerText = seconds;
+
+    if (distance < 0) {
+      clearInterval(interval);
+      document.querySelector(".countdown").innerText = "The event has started!";
+    }
+  }
+
+  const interval = setInterval(updateCountdown, 1000);
+
   return {
     head: ["<title>GoToJo 2024</title>", '<link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />'],
     type: "div",
@@ -103,6 +127,74 @@ export default function Home() {
                     ],
                   },
                   SearchBar(),
+                ],
+              },
+              {
+                type: "div",
+                props: {
+                  className: "countdown",
+                  style: {
+                    display: "flex",
+                    gap: "10px",
+                    "justify-content": "center",
+                    "align-items": "center",
+                    "margin-bottom": "20px",
+                    "background-color": "#342E46",
+                    "text-align": "center",
+                    color: "#DAA520",
+                    padding: "20px",
+                    "border-radius": "25px",
+                    "font-weight": "800"
+                  },
+                },
+                children: [
+                  {
+                    type: "div",
+                    props: {
+                      style: {
+                        "background-image": "url(./img/logo_JO.png)",
+                        "background-size": "contain",
+                        "background-repeat": "no-repeat",
+                        "width": "100px",
+                        "height": "100px",
+                      },
+                    },
+                  },
+                  {
+                    type: "div",
+                    props: { style: { "font-size": "1.5em", "max-width": "500px" } },
+                    children: [
+                      { type: "TEXT_NODE", content: "Temps restant avant le début des Jeux Olympiques Paris 2024" },
+                    ],
+                  },
+                  {
+                    type: "div",
+                    children: [
+                      { type: "span", props: { id: "days", style: { display: "block", "font-size": "2em" } } },
+                      { type: "TEXT_NODE", content: "Jours" },
+                    ],
+                  },
+                  {
+                    type: "div",
+                    children: [
+                      { type: "span", props: { id: "hours", style: { display: "block", "font-size": "2em" } } },
+                      { type: "TEXT_NODE", content: "Heures" },
+                    ],
+                  },
+                  {
+                    type: "div",
+                    children: [
+                      { type: "span", props: { id: "minutes", style: { display: "block", "font-size": "2em" } } },
+                      { type: "TEXT_NODE", content: "Minutes" },
+                    ],
+                  },
+                  {
+                    type: "div",
+                    children: [
+                      { type: "span", props: { id: "seconds", style: { display: "block", "font-size": "2em" } } },
+                      { type: "TEXT_NODE", content: "Secondes" },
+                    ],
+                  },
                 ],
               },
               InteractiveMap(map),
