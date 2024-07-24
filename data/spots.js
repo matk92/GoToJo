b570ae00f510eabf75e04687b6e2f8ae225d73c1
Remy Saguez <resaguez.2800@gmail.com>
2024-07-24 13:58:12 +0200

    cordonnée v4

--- data/spots.js
+++ data/spots.js
@@ -31,7 +31,7 @@ export default [
     sport: "Cyclisme sur route - arrivée Contre-la-montre (CRD), Natation marathon (OWS), Triathlon (TRI)",
     latitude: 48.86422088687875,
     longitude: 2.311927752133588,
-}
+},
 
   // ----------------------------------------------------------
 
@@ -44,7 +44,7 @@ export default [
     sport: "Basketball 3x3 (BK3), Breaking (BKG)",
     latitude: 48.86677060085702, 
     longitude: 2.322980555676446,
-}
+},
 {
   title: "Nonos & comestibles",
   type: "resto",
@@ -52,7 +52,7 @@ export default [
   sport: "Basketball 3x3 (BK3), Breaking (BKG)",
   latitude: 48.86726167579227,  
   longitude: 2.3211617916548763,
-}
+},
 {
   title: "Hotel Daguessau",
   type: "hotel",
@@ -60,7 +60,7 @@ export default [
   sport: "Basketball 3x3 (BK3), Breaking (BKG)",
   latitude: 48.86734943416911, 
   longitude: 2.3212113294353554,
-}
+},
 {
   title: "Hôtel de la Marine",
   type: "hotel",
@@ -68,207 +68,290 @@ export default [
   sport: "Basketball 3x3 (BK3), Breaking (BKG)",
   latitude: 48.866487477595236,  
   longitude: 2.3234785549653467,
-}
+},
+
+{
+  title: "Hôtel Saint-Florentin",
+  type: "hotel",
+  description: "hotel 4 étoiles avec chambre vue sur place de la concorde et terrasse", 
+  sport: "Basketball 3x3 (BK3), Breaking (BKG)",
+  latitude: 48.866613265714435, 
+  longitude: 2.323759773179418,
+},
+
+// ----------------------------------------------------------
+
+//--Athlétisme - arrivée Marathon (ATM), Cyclisme sur route - départ Contre-la-montre (CRD), Tir à l'arc (ARC)----------------
+
+{
+  title: "Café de l'Esplanade",
+  type: "bar",
+  description: " café avec terrasse vue sur les Invalides, 3,9 sur google",
+  sport: "Athlétisme - arrivée Marathon (ATM), Cyclisme sur route - départ Contre-la-montre (CRD), Tir à l'arc (ARC)",
+  latitude: 48.85822295296121, 
+  longitude: 2.3110649998945156,
+},
+{
+  title: "Restaurant Divellec",
+  type: "resto",
+  description: "restaurant 5 étoiles avec terrasse vue sur les Invalides",
+  sport: "Athlétisme - arrivée Marathon (ATM), Cyclisme sur route - départ Contre-la-montre (CRD), Tir à l'arc (ARC)",
+  latitude: 48.86103679049716, 
+  longitude: 2.311371178789578,
+},
+{
+  title: "Hôtel de Monaco - Résidence de l'Ambassadeur de Pologne",
+  type: "hotel",
+  description: "hotel 5 étoiles avec chambre vue sur les Invalides et terrasse",
+  sport: "Athlétisme - arrivée Marathon (ATM), Cyclisme sur route - départ Contre-la-montre (CRD), Tir à l'arc (ARC)",
+  latitude: 48.85925044771933, 
+  longitude: 2.3154349103495115,
+},
+
+// ----------------------------------------------------------
+
+//---------------Athlétisme marche (ATW), Cyclisme sur route - Course sur route (CRD)----------------
+
+{
+  title: "Francette",
+  type: "bar",
+  description: " péniche avec terrasse vue sur la Seine",
+  sport: "Athlétisme marche (ATW), Cyclisme sur route - Course sur route (CRD)",
+  latitude: 48.85878409506672, 
+  longitude: 2.291346498306723,
+
+},
+
+{
+  title: "Bateaux Parisiens",
+  type: "bar",
+  description: " péniche avec terrasse vue sur la Seine",
+  sport: "Athlétisme marche (ATW), Cyclisme sur route - Course sur route (CRD)",
+  latitude: 48.86036533836528, 
+  longitude: 2.2935546122929766,
+},
+
+{
+  title: "Bateau Boréas",
+  type: "hotel",
+  description: "péniche avec chambre vue sur la Seine et terrasse",
+  sport: "Athlétisme marche (ATW), Cyclisme sur route - Course sur route (CRD)",
+  latitude: 48.86113188612295, 
+  longitude: 2.292539308800561,
+},
+{
+  title: "Ducasse sur Seine",
+  type: "resto",
+  description: "restaurant 5 étoiles avec terrasse vue sur la Seine",
+  sport: "Athlétisme marche (ATW), Cyclisme sur route - Course sur route (CRD)",
+  latitude: 48.86053118057383, 
+  longitude: 2.2918297162523746,
+},
+
+// ----------------------------------------------------------
+
+//---------------Volley-ball de plage (VBV)----------------
+
+{
+  title: "Eiffel Tower",
+  type: "resto",
+  description: "monument avec terrasse",
+  sport: "Volley-ball de plage (VBV)",
+  latitude: 48.858324867861725, 
+  longitude: 2.294488991084519,
+},
+{
+  title: "Résidence Charles Floquet - Hotel",
+  type: "hotel",
+  description: "hotel 5 étoiles avec chambre vue sur la Tour Eiffel et terrasse",
+  sport: "Volley-ball de plage (VBV)",
+  latitude: 48.85609745403892, 
+  longitude: 2.2940809604176176,
+},
+{
+  title: "Hôtel De Fredoc",
+  type: "hotel",
+  description: "hotel 5 étoiles avec chambre vue sur la Tour Eiffel et terrasse",
+  sport: "Volley-ball de plage (VBV)",
+  latitude: 48.856501423096006, 
+  longitude: 2.3002094462815807,
+},
+
+// ----------------------------------------------------------
+
+//---------------Athlétisme - départ Marathon (ATM)----------------
+
+{
+  title: "Zara",
+  type: "shop",
+  description: "magasin de vêtements",
+  sport: "Athlétisme - départ Marathon (ATM)",
+  latitude: 48.85822544216284, 
+  longitude: 2.349561613259875,
+},
+{
+  title: "The Green Linnet",
+  type: "bar",
+  description: "pub irlandais avec terrasse vue sur lhôtel de ville",
+  sport: "Athlétisme - départ Marathon (ATM)",
+  latitude: 48.85735387545058, 
+  longitude: 2.3496699365704288,
+},
+{
+  title: "Apartment WS Hotel De Ville - Le Marais",
+  type: "hotel",
+  description: "hotel 4 étoiles avec chambre vue sur lhôtel de ville et terrasse",
+  sport: "Athlétisme - départ Marathon (ATM)",
+  latitude: 48.85783583852729, 
+  longitude: 2.34993909355828,
+},
+// ----------------------------------------------------------
+
+//---------------Para cyclisme sur route (PCRD)----------------
+{
+  title: "B&B Hotel Paris Clichy-Sous-Bois",
+  type: "hotel",
+  description: "hotel 3 étoiles avec chambre vue sur le boulvard",
+  sport: "Para cyclisme sur route (PCRD)",
+  latitude: 48.90563665313307, 
+  longitude: 2.550953516386383,
+},
+
+{
+  title: "McDonald's",
+  type: "resto",
+  description: "fast food",
+  sport: "Para cyclisme sur route (PCRD)",
+  latitude: 48.904909527017125, 
+  longitude: 2.5494770023174884,
+},
+//----------------------------------------------------------
+
+//---------------Para Equitation (PEQU)----------------
+{
+  title: "Château de Versailles",
+  type: "shop",
+  description: "château",
+  sport: "Para Equitation (PEQU)",
+  latitude: 48.80474333643212, 
+  longitude: 2.120200891226667,
+},
+
+{
+  title: "Les Fermes de Gally - Jardinerie, Marché, Épicerie",
+  type: "shop",
+  description: "marché",
+  sport: "Para Equitation (PEQU)",
+  latitude: 48.817451391023255, 
+  longitude: 2.080744580398762,
+},
+
+// ----------------------------------------------------------
+
+//---------------Voile (SAL)----------------
+
+{
+  title: "Skybar nhow",
+  type: "bar",
+  description: "bar avec terrasse vue sur la mer",
+  sport: "Voile (SAL)",
+  latitude: 43.26830383110287, 
+  longitude: 5.369294306301805,
+},
+
+{
+  title: "Restaurant Les Bains",
+  type: "resto",
+  description: "restaurant avec terrasse vue sur la mer",
+  sport: "Voile (SAL)",
+  latitude: 43.26861737969129, 
+  longitude: 5.369023181642602,
+},
+{
+  title: "Spa nhow Marseille",
+  type: "resto",
+  description: "spa avec vue sur la mer",
+  sport: "Voile (SAL)",
+  latitude: 43.26829687906851, 
+  longitude: 5.368362630667666,
+},
+{
+  title: "Hotel nhow Marseille",
+  type: "hotel",
+  description: "hotel 5 étoiles avec chambre vue sur la mer et terrasse",
+  sport: "Voile (SAL)",
+  latitude: 43.26873848928272, 
+  longitude: 5.368487005274358,
+},
+{
+  title: "Le Comptoir Marseillais",
+  type: "resto",
+  description: "restaurant avec terrasse vue sur la mer",
+  sport: "Voile (SAL)",
+  latitude: 43.26757554577569, 
+  longitude: 5.372279764372622,
+},
+{
+  title: "La Maison Mickael",
+  type: "resto",
+  description: "restaurant avec terrasse vue sur la mer",
+  sport: "Voile (SAL)",
+  latitude: 43.266544713851864, 
+  longitude: 5.372925296176448,
+},
+{
+  title: "Menu Picotin",
+  type: "resto",
+  description: "restaurant avec terrasse vue sur la mer",
+  sport: "Voile (SAL)",
+  latitude: 43.266640194129764, 
+  longitude: 5.372816373433807,
+},
+{
+  title: "Monument à Rimbaud de Jean Amado",
+  type: "shop",
+  description: "monument",
+  sport: "Voile (SAL)",
+  latitude: 43.26497938121978, 
+  longitude: 5.371773043832086,
+},
+
+// ----------------------------------------------------------
+
+//---------------Surf (SRF)----------------
+{
+ title: "A Hi'o To Mou'a",
+  type: "hotel",
+  description: "hotel 5 étoiles avec chambre vue sur la mer et terrasse",
+  sport: "Surf (SRF)", 
+  latitude: -17.856055615960795, 
+  longitude: -149.25336616916192,
+},
+{
+  title: "TEREHAUNUI TEAHUPOO",
+  type: "hotel",
+  description: "hotel 5 étoiles avec chambre vue sur la mer et terrasse",
+  sport: "Surf (SRF)",
+  latitude: -17.860360687689933, 
+  longitude: -149.24912180336239
+  },
+{
+  title: "Bungalows Havae Teahupoo",
+  type: "hotel",
+  description: "hotel 5 étoiles avec chambre vue sur la mer et terrasse",
+  sport: "Surf (SRF)",
+  latitude: -17.861202206633237, 
+  longitude: -149.24918610377264,
+},
+{
+  title: "Moana @ Teahupo'o",
+  type: "hotel",
+  description: "hotel 5 étoiles avec chambre vue sur la mer et terrasse",
+  sport: "Surf (SRF)",
+  latitude: -17.8559541234192, 
+  longitude: -149.25564829017495,
+},
 
 
   // ----------------------------------------------------------
-
-  //-----------------------------------------------------------
-  {
-    Sport: ["Basketball", "Gymnastique artistique", "Trampoline"],
-    Lieu: "Arena Bercy",
-    "Point de vue intéressant": "intérieur de la salle",
-  },
-  {
-    Sport: ["Judo", "Lutte"],
-    Lieu: "Arena Champ-de-Mars",
-    "Point de vue intéressant": "intérieur de la salle",
-  },
-  {
-    Sport: ["Boxe", "Pentathlon moderne"],
-    Lieu: "Arena Paris Nord",
-    "Point de vue intéressant": "intérieur de la salle",
-  },
-  {
-    Sport: ["Haltérophilie", "Handball", "Tennis de table", "Volleyball"],
-    Lieu: "Arena Paris Sud",
-    "Point de vue intéressant": "intérieur de la salle",
-  },
-  {
-    Sport: ["Badminton", "Gymnastique rythmique"],
-    Lieu: "Arena Porte de La Chapelle",
-    "Point de vue intéressant": "intérieur de la salle",
-  },
-  {
-    Sport: ["Natation synchronisée", "Plongeon", "Water-Polo"],
-    Lieu: "Centre Aquatique",
-    "Point de vue intéressant": "intérieur de la salle",
-  },
-  {
-    Sport: ["Tir"],
-    Lieu: "Centre National de Tir de Châteauroux",
-    "Point de vue intéressant": "compliquée d'accès",
-  },
-  {
-    Sport: ["Pentathlon moderne", "Sports Équestres"],
-    Lieu: "Château de Versailles",
-    "Point de vue intéressant": "château de versailles",
-    latitude: "48.8048",
-    longitude: "2.1204",
-  },
-  {
-    Sport: ["VTT"],
-    Lieu: "Colline d'Elancourt",
-    "Point de vue intéressant": "colline d'elancourt",
-    latitude: "48.7700",
-    longitude: "1.9590",
-  },
-  {
-    Sport: ["Escrime", "Taekwondo"],
-    Lieu: "Grand Palais",
-    "Point de vue intéressant": "intérieur de la salle",
-  },
-  {
-    Sport: ["Athlétisme"],
-    Lieu: "Hôtel de Ville",
-    "Point de vue intéressant": "hôtel de ville",
-    latitude: "48.8566",
-    longitude: "2.3522",
-  },
-  {
-    Sport: ["Athlétisme", "Cyclisme sur route", "Tir à l'arc"],
-    Lieu: "Invalides",
-    "Point de vue intéressant": "invalides",
-    latitude: "48.8584",
-    longitude: "2.3123",
-  },
-  {
-    Sport: ["Basketball 3x3", "Breaking", "Cyclisme BMX Freestyle", "Skateboard"],
-    Lieu: "Parc Urbain La Concorde",
-    "Point de vue intéressant": "Hôtel de Crillon, A Rosewood Hotel",
-    latitude: "48.8656",
-    longitude: "2.3214",
-  },
-  {
-    Sport: ["Golf"],
-    Lieu: "Golf National",
-    "Point de vue intéressant": "golf national",
-    latitude: "48.7710",
-    longitude: "2.0330",
-  },
-  {
-    Sport: ["Voile"],
-    Lieu: "Marina de Marseille",
-    "Point de vue intéressant": ["Hotel nhow Marseille", "La Maison Mickael", "Le Comptoir Marseillais"],
-    "latitude Hotel nhow Marseille": "43.2960",
-    "longitude Hotel nhow Marseille": "5.3650",
-    "latitude La Maison Mickael": "43.2960",
-    "longitude La Maison Mickael": "5.3650",
-    "latitude Le Comptoir Marseillais": "43.2960",
-    "longitude Le Comptoir Marseillais": "5.3650",
-  },
-  {
-    Sport: ["Football"],
-    Lieu: "Parc des Princes",
-    "Point de vue intéressant": "Parc des Princes",
-  },
-  {
-    Sport: ["Natation", "Water-Polo"],
-    Lieu: "Paris La Défense Arena",
-    "Point de vue intéressant": "Paris La Défense Arena",
-  },
-  {
-    Sport: ["Cyclisme sur route", "Marathon Swimming", "Triathlon"],
-    Lieu: "Pont Alexandre III",
-    "Point de vue intéressant": "Pont Alexandre III",
-    latitude: "48.8636",
-    longitude: "2.3136",
-  },
-  {
-    Sport: ["Escalade sportive"],
-    Lieu: "Site d'escalade Bourget",
-    "Point de vue intéressant": "Site d'escalade Bourget",
-  },
-  {
-    Sport: ["Cycling BMX Racing"],
-    Lieu: "Stade BMX de Saint-Quentin-en-Yvelines",
-    "Point de vue intéressant": "Stade BMX de Saint-Quentin-en-Yvelines",
-  },
-  {
-    Sport: ["Football"],
-    Lieu: "Stade de Bordeaux",
-    "Point de vue intéressant": "Stade de Bordeaux",
-  },
-  {
-    Sport: ["Athlétisme", "Rugby Sevens"],
-    Lieu: "Stade de France",
-    "Point de vue intéressant": "Stade de France",
-  },
-  {
-    Sport: ["Football"],
-    Lieu: "Stade de la Beaujoire",
-    "Point de vue intéressant": "Stade de la Beaujoire",
-  },
-  {
-    Sport: ["Football"],
-    Lieu: "Stade de Lyon",
-    "Point de vue intéressant": "Stade de Lyon",
-  },
-  {
-    Sport: ["Football"],
-    Lieu: "Stade de Marseille",
-    "Point de vue intéressant": "Stade de Marseille",
-  },
-  {
-    Sport: ["Football"],
-    Lieu: "Stade de Nice",
-    "Point de vue intéressant": "Stade de Nice",
-  },
-  {
-    Sport: ["Football"],
-    Lieu: "Stade Geoffroy-Guichard",
-    "Point de vue intéressant": "Stade Geoffroy-Guichard",
-  },
-  {
-    Sport: ["Aviron", "Canoe Flatwater", "Canoe Slalom"],
-    Lieu: "Stade Nautique de Vaires-sur-Marne",
-    "Point de vue intéressant": "Stade Nautique de Vaires-sur-Marne",
-  },
-  {
-    Sport: ["Basketball", "Handball"],
-    Lieu: "Stade Pierre Mauroy",
-    "Point de vue intéressant": "Stade Pierre Mauroy",
-  },
-  {
-    Sport: ["Boxe", "Tennis"],
-    Lieu: "Stade Roland-Garros",
-    "Point de vue intéressant": "Stade Roland-Garros",
-  },
-  {
-    Sport: ["Volleyball de plage"],
-    Lieu: "Stade Tour Eiffel",
-    "Point de vue intéressant": ["tour eiffel", "Hôtel Pullman Paris Tour Eiffel"],
-    "Cordonnées tour eiffel": "48.8584, 2.2945",
-    "Cordonnées Hôtel Pullman Paris Tour Eiffel": "48.8584, 2.2945",
-  },
-  {
-    Sport: ["Hockey"],
-    Lieu: "Stade Yves-du-Manoir",
-    "Point de vue intéressant": "",
-  },
-  {
-    Sport: ["Surf"],
-    Lieu: "Teahupo'o, Tahiti",
-    "Point de vue intéressant": "",
-  },
-  {
-    Sport: ["Athlétisme", "Cyclisme sur route"],
-    Lieu: "Trocadéro",
-    "Point de vue intéressant": "",
-  },
-  {
-    Sport: ["Cyclisme sur piste"],
-    Lieu: "Vélodrome National de Saint-Quentin-en-Yvelines",
-    "Point de vue intéressant": "",
-  },
 ];
