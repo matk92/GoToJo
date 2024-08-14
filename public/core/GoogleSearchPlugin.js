d4e048b1a51d1be0c89ddc5a5dfeb1786db3d8ab
Nico Calvelo <dia.calvelo.nicolas@gmail.com>
2024-08-14 19:31:36 +0200

    delete key

--- public/core/GoogleSearchPlugin.js
+++ public/core/GoogleSearchPlugin.js
@@ -1,11 +1,9 @@
 class GoogleSearchPlugin {
   search = async (search) => {
     return new Promise((resolve, reject) => {
-      // AIzaSyCHzRzxCzN42920CeHDKr1oLnTKciqDIpU
-      // AIzaSyA3PYeK4r1vnaCPqQ7hC4a9XjGmfqg_4_0
       const encodedSearch = encodeURIComponent(search.trim().toLowerCase());
       fetch(
-        "https://www.googleapis.com/customsearch/v1?key=AIzaSyCHzRzxCzN42920CeHDKr1oLnTKciqDIpU&cx=639a89b0e9e4343b0&q=" +
+        "https://www.googleapis.com/customsearch/v1?key=&cx=639a89b0e9e4343b0&q=" +
           encodedSearch
       )
         .then((response) => response.json())
@@ -18,7 +16,7 @@ class GoogleSearchPlugin {
     new Promise((resolve, reject) => {
       const encodedSearch = encodeURIComponent(search.trim().toLowerCase());
       fetch(
-        "https://www.googleapis.com/customsearch/v1?key=AIzaSyCHzRzxCzN42920CeHDKr1oLnTKciqDIpU&cx=639a89b0e9e4343b0&q=" +
+        "https://www.googleapis.com/customsearch/v1?key=&cx=639a89b0e9e4343b0&q=" +
           encodedSearch +
           "&searchType=image"
       )
